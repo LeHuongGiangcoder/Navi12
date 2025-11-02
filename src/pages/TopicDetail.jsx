c538ebd865703aaca7355c3f77697f7104142df3
LeHuongGiangcoder <hgiangle.work@gmail.com>
2025-11-02 16:03:06 +0700

    oki

--- src/pages/TopicDetail.jsx
+++ src/pages/TopicDetail.jsx
@@ -102,6 +102,7 @@ const TopicDetail = () => {
                 ></div>
               </div>
               <span className="progress-text">{topicProgress.completed}% hoàn thành</span>
+              <span className="top-skill">Kỹ năng của bạn thuộc top 5%</span>
             </div>
           </div>
           <p className="topic-description">{topic.description}</p>
